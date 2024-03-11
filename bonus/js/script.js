17a40bba00fc2c4d0c9db9ef4905d80372a6af93
anto-iorfida <antonioiorfida@hotmail.com>
2024-03-11 15:56:49 +0100

    Feat add bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,7 @@
+// Dato un array contenente 10 numeri, stampare in console il numero più grande. 
+
+// creare un array con 10 numeri 
+const num = [1,3,4,9,2,5,6,8,5,4];
+
+let numBigger = Math.max(...num);
+console.log('Il numero più grande è:', numBigger);
