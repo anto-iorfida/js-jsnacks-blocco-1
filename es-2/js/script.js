ed18b34470e0fab187924a59aab8ec14a2b7ef76
anto-iorfida <antonioiorfida@hotmail.com>
2024-03-11 15:15:03 +0100

    Feat : created  cycle four for ask user a number

--- es-2/js/script.js
+++ es-2/js/script.js
@@ -1,1 +1,10 @@
 // Crea un array vuoto.Chiedi per 6 volte all’utente di inserire un numero,se è dispari inseriscilo nell’array.
+
+// creo l'array vuoto
+let contenitorNum = [];
+
+// chiedo un numero 6 volte all'utente
+for (let i = 0 ; i < 6 ; i++){
+    const userNum = parseInt(prompt('inserisci un numero'))
+    console.log(userNum)
+}
